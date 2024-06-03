4ee57cdad295124d279cd84cb3231b92eaed30aa
Eryc Jacinto <121405251+ErycMJ@users.noreply.github.com>
2024-06-02 21:17:27 -0300

    Update Chat.jsx

--- new-chat/src/chat/Chat.jsx
+++ new-chat/src/chat/Chat.jsx
@@ -1,26 +1,13 @@
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 export default function Chat() {
   const [items, setItems] = useState([]);
   const [message, setMessage] = useState("");
-  const [user, setUser] = useState('usuario');
+  const [user, setUser] = useState("usuario");
   const navigate = useNavigate();
-
-  async function addItem(event) {
-    event.preventDefault();
-
-    if (message.trim() !== "") {
-      const newItem = {
-        id: Date.now(),
-        text: message,
-        user: user,
-      };
-
-      setItems((prevItems) => [...prevItems, newItem]);
-      setMessage(""); // Limpa o campo de entrada após adicionar a mensagem
-    }
-  }
+  const location = useLocation();
+  const userName = location.state?.userName || "Usuário";
 
   useEffect(() => {
     const _lista = localStorage.getItem("_lista");
@@ -38,12 +25,27 @@ export default function Chat() {
     }
   }, [items]);
 
+  function addItem(event) {
+    event.preventDefault();
+
+    if (message.trim() !== "") {
+      const newItem = {
+        id: Date.now(),
+        text: message,
+        user: user,
+      };
+
+      setItems((prevItems) => [...prevItems, newItem]);
+      setMessage("");
+    }
+  }
+
   function trocarUsuario() {
-    setUser((prevUser) => (prevUser === 'usuario' ? 'atendente' : 'usuario'));
+    setUser((prevUser) => (prevUser === "usuario" ? "atendente" : "usuario"));
   }
 
   function limparMensagens() {
-    setItems([]); // Limpa todas as mensagens
+    setItems([]);
   }
 
   return (
@@ -53,6 +55,9 @@ export default function Chat() {
           <h2 className="mb-5 text-center text-3xl font-semibold text-gray-800">
             Atendimento Online
           </h2>
+          <p className="mb-5 text-center font-semibold text-gray-800">
+            Chat com nossos atendentes
+          </p>
           <div className="mb-4 flex justify-between">
             <button
               type="button"
@@ -69,15 +74,17 @@ export default function Chat() {
               Apagar
             </button>
           </div>
-          <p className="mb-5 text-center font-semibold text-gray-800">
-            Chat com nossos atendentes
-          </p>
-          <ul className="mb-4 max-h-60 overflow-y-auto rounded border p-2">
+          <ul className="mb-4 min-h-60 max-h-60 overflow-y-auto rounded border p-2">
             {items.map((item) => (
-              <li key={item.id} className={`mb-2 rounded p-2 ${item.user === 'usuario' ? 'bg-gray-200' : 'bg-blue-200'}`}>
-                {item.user === 'usuario' ? 'Você diz:' : 'Atendente diz:'}
-                <br />
-                {item.text}
+              <li
+                key={item.id}
+                className={`flex ${item.user === 'usuario' ? 'justify-end items-end' : 'justify-start items-start'}`}
+              >
+                <div className={`p-2 break-words rounded-xl min-w-44 max-w-44 m-1 ${item.user === 'usuario' ? 'bg-blue-200' : 'bg-gray-200'}`}>
+                  <span className="font-semibold">{item.user === "usuario" ? `${userName}` : "Atendente"}</span>
+                  <br />
+                  {item.text}
+                </div>
               </li>
             ))}
           </ul>
@@ -94,14 +101,14 @@ export default function Chat() {
               />
               <button
                 type="submit"
-                className="rounded bg-blue-500 px-4 py-2 font-semibold text-white hover:bg-blue-600"
+                className="shadow-sm rounded bg-blue-500 px-4 py-2 font-semibold text-white hover:bg-blue-600"
               >
                 Enviar
               </button>
               <button
                 type="button"
                 onClick={trocarUsuario}
-                className="rounded bg-white px-4 py-2 font-semibold text-black hover:bg-gray-300"
+                className="shadow rounded bg-white px-4 py-2 font-semibold text-black hover:bg-gray-300"
               >
                 Trocar
               </button>
